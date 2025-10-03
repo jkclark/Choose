2b1dfef59f5ce8fa5cadaea2b7ec5bc91c9f18d8
Josh Clark <jclarktennis@gmail.com>
2025-10-03 14:28:58 -0400

    feat: add background proprotional to percentage

--- frontend/src/components/ChooseItem.tsx
+++ frontend/src/components/ChooseItem.tsx
@@ -14,16 +14,28 @@ const ChooseItem: React.FC<ChooseItemProps> = ({
   return (
     <div
       className={[
-        "group flex aspect-[1/2] w-full items-center justify-center rounded-md",
+        "group relative flex aspect-[1/2] w-full items-center justify-center overflow-hidden rounded-md",
         "border border-black transition-colors select-none",
         !userChose ? "cursor-pointer hover:border-gray-100" : "",
         chosenIndex === index ? "border-gray-100" : "",
       ].join(" ")}
       onClick={handleClick}
     >
+      {/* Animated background */}
+      <div
+        className={[
+          "absolute inset-x-0 bottom-0 bg-gray-600 transition-all duration-1000 ease-out",
+          userChose ? "opacity-70" : "opacity-0",
+        ].join(" ")}
+        style={{
+          height: userChose ? `${percentChosen}%` : "0%",
+        }}
+      />
+
+      {/* Percentage text */}
       <span
         className={[
-          "text-lg text-white transition-opacity duration-350 ease-in-out",
+          "relative z-10 text-lg text-white transition-opacity duration-350 ease-in-out",
           userChose ? "opacity-100" : "opacity-0",
         ].join(" ")}
       >
